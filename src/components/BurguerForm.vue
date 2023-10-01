11a8d61d126785615bb32e2d9451d53f5223b1bc
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-10-01 02:59:38 -0300

    feat: criando formulários de cadastro part II
    Trazendo itens para seleção do banco de dados

--- src/components/BurguerForm.vue
+++ src/components/BurguerForm.vue
@@ -11,74 +11,107 @@
                     <label for="pao">Escolha o pão:</label>
                     <select name="pao" id="pao" v-model="pao">
                         <option value="">Selecione seu pão</option>
-                        <option value="integral">Integral</option>
+                        <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">
+                            {{ pao.tipo }}
+                        </option>
                     </select>
                 </div>
                 <div class="input-container">
                     <label for="carne">Escolha a carne do seu burguer:</label>
                     <select name="carne" id="carne" v-model="carne">
-                        <option value="">Selecione o corte:</option>
-                        <option value="maminha">Maminha</option>
+                        <option value="">Selecione a carne</option>
+                        <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">
+                            {{ carne.tipo }}
+                        </option>
                     </select>
                 </div>
                 <div id="opcionais-container" class="input-container">
-                    <label id="opcionais-title" for="opcionais">Escolha a carne do seu burguer:</label>
-                    <div class="checkbox-container">
-                        <input type="checkbox" name="opcionais" v-model="opcinais" value="bacon">
-                        <span>Bacon</span>
-                    </div>             
+                    <label id="opcionais-title" for="opcionais">Escolha os opcionais:</label>
+                    <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id">
+                        <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
+                        <span>{{ opcional.tipo }}</span>
+                    </div>
                 </div>
                 <div class="input-container">
                     <input type="submit" class="submit-btn" value="Criar meu burguer">
                 </div>
             </form>
         </div>
-
     </div>
 </template>
 
 <script>
 export default {
-    name: "BurguerForm"
+    name: "BurguerForm",
+    data() {
+        return {
+            paes: null,
+            carnes: null,
+            opcionaisdata: null,
+            nome: null,
+            pao: null,
+            carne: null,
+            opcionais: [],
+            status: "Solicitado",
+            msg: null
+        }
+    },
+    methods: {
+        async getIngredientes() {
+
+            const req = await fetch("http://localhost:3000/ingredientes");
+            const data = await req.json();
+
+            console.log(data);
+
+            this.paes = data.paes;
+            this.carnes = data.carnes;
+            this.opcionaisdata = data.opcionais;
+        }
+    },
+    mounted() {
+        this.getIngredientes()
+    }
 }
 
 </script>
 
 <style scoped>
-#burguer-form{
+#burguer-form {
     max-width: 400px;
     margin: 0 auto;
 }
 
-.input-container{
+.input-container {
     display: flex;
     flex-direction: column;
     margin-bottom: 20px;
 }
 
-label{
+label {
     font-weight: bold;
     margin-bottom: 15px;
-    color:#222;
-    padding:5px 10px;
+    color: #222;
+    padding: 5px 10px;
     border-left: 4px solid #FCBA03;
 }
 
-input, select{
+input,
+select {
     padding: 5px 10px;
     width: 300px;
 }
 
-#opcinais-container{
+#opcionais-container {
     flex-direction: row;
     flex-wrap: wrap;
 }
 
-#opcinais-title{
+#opcionais-title {
     width: 100%;
 }
 
-.checkbox-container{
+.checkbox-container {
     display: flex;
     align-items: flex-start;
     width: 50%;
@@ -86,16 +119,16 @@ input, select{
 }
 
 .checkbox-container span,
-.checkbox-container input{
+.checkbox-container input {
     width: auto;
 }
 
-.checkbox-container span{
+.checkbox-container span {
     margin-left: 6px;
     font-weight: bold;
 }
 
-.submit-btn{
+.submit-btn {
     background-color: #222;
     color: #FCBA03;
     font-weight: bold;
@@ -107,7 +140,7 @@ input, select{
     transition: .5s;
 }
 
-.submit-btn:hover{
+.submit-btn:hover {
     background-color: transparent;
     color: #222;
 }
