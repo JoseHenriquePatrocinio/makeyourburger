e234bd05c84a61ad9ec30152328c27c66c0f54b0
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-10-01 03:35:22 -0300

    feat: possibilitando inserção de itens no banco de dados

--- src/components/BurguerForm.vue
+++ src/components/BurguerForm.vue
@@ -2,7 +2,7 @@
     <div>
         <p>Componente de mensagem</p>
         <div>
-            <form id="burguer-form">
+            <form id="burguer-form" @submit="createBurguer">
                 <div class="input-container">
                     <label for="nome">Nome do Cliente:</label>
                     <input type="text" id="nome" v-model="nome" placeholder="Digite o seu nome">
@@ -67,6 +67,32 @@ export default {
             this.paes = data.paes;
             this.carnes = data.carnes;
             this.opcionaisdata = data.opcionais;
+        },
+        async createBurguer(e) {
+            e.preventDefault();
+
+            const data = {
+                nome:this.nome,
+                carne: this.carne,
+                pao: this.pao,
+                opcionais:Array.from(this.opcionais),
+                status: "Solicitado"
+            }
+            const dataJson = JSON.stringify(data);
+
+            const req = await fetch("http://localhost:3000/burgers",{
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: dataJson
+            });
+
+            const res = await req.json();
+
+            this.nome = "";
+            this.carne = "";
+            this.pao = "";
+            this.opcionais = "";
+
         }
     },
     mounted() {
