c010222f234c53a34b781a087b63688a83260534
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-10-01 20:41:26 -0300

    feat: resgatando pedidos do banco de dados

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,15 +11,14 @@
             </div>
         </div>
         <div id="burger-table-rows">
-            <div class="burger-table-row">
-                <div class="order-number">1</div>
-                <div>João</div>
-                <div>Pão Italiano</div>
-                <div>Maminha</div>
+            <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+                <div class="order-number">{{ burger.id }}</div>
+                <div>{{ burger.nome }}</div>
+                <div>{{ burger.pao }}</div>
+                <div>{{ burger.carne }}</div>
                 <div>
                     <ul>
-                        <li>Salame</li>
-                        <li>Tomate</li>
+                        <li v-for="(opcional,index) in burger.opcionais" :key="index">{{ opcional }}</li>
                     </ul>
                 </div>
                 <select name="status" id="status">
@@ -33,52 +32,70 @@
 
 <script>
 export default {
-    name: "Dashboard"
+    name: "Dashboard",
+    data() {
+        return {
+            burgers: null,
+            burger_id: null,
+            status: []
+        }
+    },
+    methods: {
+        async getPedidos() {
+            const req = await fetch("http://localhost:3000/burgers");
+            const data = await req.json();
+            this.burgers = data;
+
+            console.log(this.burgers)
+        }
+    },
+    mounted() {
+        this.getPedidos();
+    }
 }
 </script>
 
 <style scoped>
-
-#burger-table{
+#burger-table {
     max-width: 1200px;
     margin: 0 auto;
 }
 
 #burger-table-heading,
 #burger-table-rows,
-.burger-table-row{
+.burger-table-row {
     display: flex;
     flex-wrap: wrap;
 }
 
-#burger-table-heading{
+#burger-table-heading {
     font-weight: bold;
     padding: 12px;
     border-bottom: 3px solid #333;
 }
 
 #burger-table-heading div,
-.burger-table-row div{
+.burger-table-row div {
     width: 19%;
 }
 
-.burger-table-row{
+.burger-table-row {
     width: 100%;
     padding: 12px;
     border-bottom: 1px solid #CCC;
 }
 
 #burger-table-heading .order-id,
-.burger-table-row .order-number{
+.burger-table-row .order-number {
     width: 5%;
 }
 
-select{
+select {
     padding: 12px 6px;
     margin-right: 12px;
 }
 
-.delete-btn{
+.delete-btn {
     background-color: #222;
     color: #FCBA03;
     font-weight: bold;
@@ -90,10 +107,8 @@ select{
     transition: .5s;
 }
 
-.delete-btn:hover{
+.delete-btn:hover {
     background-color: transparent;
     color: #222;
 }
-
-
 </style>
